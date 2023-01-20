68263b9638642c6f677da22802dc7dfac43dc674
Tom Bewley <tom.bewley@bristol.ac.uk>
2023-01-20 11:55:03 +0000

    Spatial splitting fully implemented

--- csta/csta.py
+++ csta/csta.py
@@ -6,27 +6,57 @@ class CSTA:
     """
     Class for performing contrastive spatiotemporal abstraction.
     """
-    def __init__(self, temporal_dims: list[str], spatial_dims: list[str]):
+    def __init__(self, temporal_dims: list[str], temporal_split_thresholds: list[np.ndarray],
+                 spatial_dims: list[str], spatial_split_thresholds: list[np.ndarray]):
+        assert len(temporal_split_thresholds) == len(temporal_dims), "Must specify split thresholds for every dim."
+        assert len(spatial_split_thresholds) == len(spatial_dims), "Must specify split thresholds for every dim."
         self.T = HRSubset(dims=temporal_dims)
+        self.temporal_split_thresholds = temporal_split_thresholds
         self.X = HRSubset(dims=spatial_dims)
+        self.spatial_split_thresholds = spatial_split_thresholds
 
     @property
     def n(self): return len(self.T.leaves)
     @property
     def m(self): return len(self.X.leaves)
+    @property
+    def d_t(self): return len(self.T.dims)
+    @property
+    def d_s(self): return len(self.X.dims)
 
     def transition_mask(self, timestamps: np.ndarray, states: np.ndarray, next_states: np.ndarray):
-        return transition_mask(w_bounds_array=np.array([w.bounds for w in self.T.leaves]),
-                               x_bounds_array=np.array([x.bounds for x in self.X.leaves]),
-                               timestamps=timestamps, states=states, next_states=next_states
-                               )
+        num = timestamps.shape[0]
+        assert states.shape[0] == next_states.shape[0] == num, "Arrays must have common length."
+        l_T, l_X = self.T.leaves, self.X.leaves
+        mask = np.zeros((len(l_T), len(l_X), len(l_X), num), dtype=np.bool_)
+        def _recurse(w, x, xx, indices):
+            w_l = w_r = w; x_l = x_r = x; xx_l = xx_r = xx
+            if not w.is_leaf:
+                right = w.sort(timestamps[indices])
+                w_l, w_r = w.left, w.right
+            elif not x.is_leaf:
+                right = x.sort(states[indices])
+                x_l, x_r = x.left, x.right
+            elif not xx.is_leaf:
+                right = xx.sort(next_states[indices])
+                xx_l, xx_r = xx.left, xx.right
+            else:
+                mask[l_T.index(w), l_X.index(x), l_X.index(xx), indices] = True
+                return
+            _recurse(w_l, x_l, xx_l, indices[~right])
+            _recurse(w_r, x_r, xx_r, indices[ right])
+        _recurse(self.T, self.X, self.X, np.arange(num))
+        return mask
 
-    # def set_windows(self, *thresholds):
-    #     self.T.merge()
-    #     for threshold in sorted(thresholds):
-    #         self.T.leaves[-1].split(0, threshold)
+    def set_windows(self, *thresholds):
+        self.T.merge()
+        leaf = self.T
+        for threshold in sorted(thresholds):
+            leaf.split(0, threshold)
+            leaf = leaf.right
 
-    def eval_splits(self, timestamps: np.ndarray, states: np.ndarray, next_states: np.ndarray, temporal: bool = False):
+    def eval_splits(self, timestamps: np.ndarray, states: np.ndarray, next_states: np.ndarray,
+                    temporal: bool = False, debug: bool = False):
         n, m = self.n, self.m
         mask_current = self.transition_mask(timestamps, states, next_states)
         counts_current = mask_current.sum(axis=3)
@@ -34,53 +64,85 @@ class CSTA:
         if temporal:
             assert m > 1, "Temporal abstraction requires at least two abstract states."
             w_mask_current = mask_current.sum(axis=(1, 2)).astype(bool)
-            qual = []
+            gains = []
             raise NotImplementedError
         else:
             assert n > 1, "Spatial abstraction requires at least two time windows."
             x_mask_current = mask_current.sum(axis=(0, 2)).astype(bool)
             xx_mask_current = mask_current.sum(axis=(0, 1)).astype(bool)
-            qual = []
-            for x in numba.prange(len(self.X.leaves)):
-                qual.append([])
+            gains = [[] for _ in range(m)]
+            for x in range(m):
+                bounds = self.X.leaves[x].bounds
+                # Define expanded mask and counts arrays
                 mask_exp = np.insert(mask_current, x + 1, 0, axis=1)
-                mask_exp = np.insert(mask_exp,     x + 1, 0, axis=2)
+                mask_exp = np.insert(mask_exp, x + 1, 0, axis=2)
                 counts_exp = np.insert(counts_current, x + 1, 0, axis=1)
                 counts_exp = np.insert(counts_exp, x + 1, 0, axis=2)
-                for dim in numba.prange((len(self.X.dims))):
-
-                    thresholds = np.linspace(0.1, 9.9, 100)
-
-                    move_x  = geq_threshold_mask(states,      x_mask_current[x],  dim, thresholds)
-                    move_xx = geq_threshold_mask(next_states, xx_mask_current[x], dim, thresholds)
+                for dim in range(self.d_s):
+                    # Valid thresholds are those that lie within the bounds of this leaf
+                    th = self.spatial_split_thresholds[dim]
+                    valid_thresholds = th[np.logical_and(bounds[0, dim] < th, th < bounds[1, dim])]
+                    # Compare all state values to all valid thresholds in parallel
+                    move_x = geq_threshold_mask(states, x_mask_current[x], dim, valid_thresholds)
+                    move_xx = geq_threshold_mask(next_states, xx_mask_current[x], dim, valid_thresholds)
                     move_both = np.logical_and(move_x, move_xx)
                     move_x_only = np.logical_and(move_x, ~move_xx)
                     move_xx_only = np.logical_and(~move_x, move_xx)
-
-                    delta = np.zeros((len(thresholds), n, m + 1, m + 1), dtype=np.int_)
-                    for c in numba.prange(len(thresholds)):
-
+                    delta = np.zeros((len(valid_thresholds), n, m + 1, m + 1), dtype=np.int_)
+                    for c in numba.prange(len(valid_thresholds)):
+                        # Handle transitions where the first state is in the right child
                         delta_x_only = mask_exp[:, x, :, move_x_only[c]].sum(axis=0)
                         delta[c, :, x, :] -= delta_x_only
                         delta[c, :, x + 1, :] += delta_x_only
-
+                        # Handle transitions where the second state is in the right child
                         delta_xx_only = mask_exp[:, :, x, move_xx_only[c]].sum(axis=2)
                         delta[c, :, :, x] -= delta_xx_only
                         delta[c, :, :, x + 1] += delta_xx_only
-
+                        # Handle transitions where both states are in the right child
                         delta_both = mask_exp[:, x, x, move_both[c]].sum(axis=1)
                         delta[c, :, x, x] -= delta_both
                         delta[c, :, x + 1, x + 1] += delta_both
-
                         assert (delta[c].sum(axis=(1, 2)) == 0).all()
-                        # TODO: More assertions
-
-                    qual[x].append(jsd(counts_exp + delta) - jsd_current)
-
-        return qual
+                    # Record Jensen-Shannon divergence for each valid threshold
+                    gains[x].append((valid_thresholds, _jsd(counts_exp + delta) - jsd_current))
+                    # Check that computed counts match that after split is made
+                    if debug:
+                        greedy = np.argmax(gains[x][-1][1])
+                        print(f"greedy: leaf {x}, {dim} = {valid_thresholds[greedy]} (JSD = {gains[x][-1][1][greedy]})")
+                        parent = self.X.leaves[x]
+                        parent.split(dim, valid_thresholds[greedy])
+                        match = (self.transition_mask(timestamps, states, next_states).sum(axis=3)
+                                 == (counts_exp + delta[greedy])).all()
+                        assert match
+                        parent.merge()
+        return gains
 
     def eval_merges(self):
         """MCCP"""
+
+    def make_greedy_change(self, timestamps: np.ndarray, states: np.ndarray, next_states: np.ndarray):
+
+        alpha = 0.
+
+        spatial_split_gains = self.eval_splits(timestamps, states, next_states, temporal=False)
+        candidates = {}
+        for x in range(self.m):
+            for dim in range(self.d_s):
+                g = spatial_split_gains[x][dim]
+                greedy = np.argmax(g[1])
+                qual = g[1][greedy] - alpha
+                if qual > 0:
+                    candidates[(x, dim, g[0][greedy])] = qual
+
+        if len(candidates) == 0:
+            print("Local optimum reached")
+        else:
+            key = max(candidates, key=candidates.get)
+            x, dim, threshold = key
+            print(f"Split abstract state {x} at dim {dim} = {threshold}")
+            self.X.leaves[x].split(dim, threshold)
+
+        return spatial_split_gains
 
 
 class HRSubset:
@@ -93,23 +155,28 @@ class HRSubset:
             self.bounds = bounds
         self.merge()
 
-    def __str__(self):
-        return ", ".join([f"{l} <= {dim} < {u}" for dim, (l, u) in zip(self.dims, self.bounds.T)])
+    def __repr__(self):
+        return "(" + " and ".join([f"{l} <= {dim} < {u}" for dim, (l, u) in zip(self.dims, self.bounds.T)]) + ")"
+
+    @property
+    def is_leaf(self):
+        return self.split_dim is None
 
     @property
     def leaves(self):
-        if self.split_dim is None:
+        if self.is_leaf:
             return [self]
         return self.left.leaves + self.right.leaves
 
-    def contains(self, data: np.ndarray):
-        return in_bounds_mask(self.bounds, data)
+    def sort(self, data: np.ndarray):
+        assert not self.is_leaf, "Subset hasn't been split."
+        return data[..., self.split_dim] >= self.split_threshold
 
     def merge(self):
         self.split_dim, self.split_threshold, self.left, self.right = None, None, None, None
 
     def split(self, dim: int, threshold: float):
-        assert self.split_dim is None, "Subset has already been split."
+        assert self.is_leaf, "Subset has already been split."
         assert 0 <= dim < len(self.dims), f"Split dimension ({dim}) out of range."
         assert self.bounds[0, dim] <= threshold < self.bounds[1, dim], f"Split (dim {dim} = {threshold}) out of bounds."
         self.split_dim = dim
@@ -118,35 +185,6 @@ class HRSubset:
         bounds_left[1, dim] = bounds_right[0, dim] = threshold
         self.left = HRSubset(self.dims, bounds_left)
         self.right = HRSubset(self.dims, bounds_right)
-
-
-@numba.jit(nopython=True, cache=True, parallel=True)
-def in_bounds_mask(bounds, data):
-    mask = np.zeros(data.shape[0], dtype=np.bool_)
-    for i in numba.prange(data.shape[0]):
-        if (bounds[0] <= data[i]).all() and (data[i] < bounds[1]).all():
-            mask[i] = True
-    return mask
-
-
-@numba.jit(nopython=True, cache=True, parallel=True)
-def transition_mask(w_bounds_array: np.ndarray, x_bounds_array: np.ndarray,
-                     timestamps: np.ndarray, states: np.ndarray, next_states: np.ndarray):
-    num = timestamps.shape[0]
-    assert states.shape[0] == next_states.shape[0] == num, "Arrays must have common length."
-    n, m = w_bounds_array.shape[0], x_bounds_array.shape[0]
-    mask = np.zeros((n, m, m, num), dtype=np.bool_)
-    idx_range = np.arange(num)
-    for w in numba.prange(n):
-        mask_w = in_bounds_mask(w_bounds_array[w], timestamps)
-        for x in numba.prange(m):
-            mask_x = in_bounds_mask(x_bounds_array[x], states[mask_w])
-            for xx in numba.prange(m):
-                idx_range_here = idx_range[mask_w][mask_x]
-                mask_here = in_bounds_mask(x_bounds_array[xx], next_states[mask_w][mask_x])
-                for i in numba.prange(len(idx_range_here)):
-                    mask[w, x, xx, idx_range_here[i]] = mask_here[i]
-    return mask
 
 
 @numba.jit(nopython=True, cache=True, parallel=True)
@@ -162,12 +200,10 @@ def jsd(counts: np.ndarray):
     return _jsd(counts)
 
 
-# @numba.jit(nopython=True, cache=True)
+@numba.jit(nopython=True, cache=True)
 def _jsd(counts: np.ndarray):
-    def entropy(p):  # https://stackoverflow.com/a/65675506
-        plogp = (-p * np.log(p)).ravel()
-        plogp[np.isnan(plogp)] = 0  # -p*log(p) undefined at 0
-        return plogp.reshape(p.shape).sum(axis=-1)
+    def entropy(p):
+        return (-p * np.where(p > 0, np.log(p), 0)).sum(axis=-1)
     r, n, m, _ = counts.shape
     counts = counts.reshape(r, n, m*m)  # Flatten 2D representation of joint distribution
     counts_total = counts.sum(axis=-1)
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
-from csta.csta import *
+from csta.csta import CSTA
 
 
 df = pd.read_csv(f"data/maze-RL.csv")
@@ -13,13 +14,30 @@ all_states = df[["x", "y"]].values
 states = all_states[not_first - 1]
 next_states = all_states[not_first]
 
-model = CSTA(spatial_dims=["x", "y"], temporal_dims=["ep", "time"])
+model = CSTA(temporal_dims=["ep", "time"],
+             temporal_split_thresholds=[np.arange(1, 750), np.array([])],
+             spatial_dims=["x", "y"],
+             spatial_split_thresholds=[np.linspace(0.1, 9.9, 100), np.linspace(0.1, 9.9, 100)],
+             )
 
-model.T.split(0, 375)
+model.set_windows(*np.arange(1, max(df["ep"])))
+# model.set_windows(375)
 
-qual = model.eval_splits(timestamps, states, next_states)
+model.make_greedy_change(timestamps, states, next_states)
+model.make_greedy_change(timestamps, states, next_states)
+model.make_greedy_change(timestamps, states, next_states)
+gain = model.make_greedy_change(timestamps, states, next_states)
 
+for x in model.X.leaves:
+    print(x)
 
-plt.plot(qual[0][0])
-plt.plot(qual[0][1])
-plt.show()
+# m = model.m - 1
+# num_rows = int(np.sqrt(m))
+# num_cols = int(np.ceil(m / num_rows))
+# _, axes = plt.subplots(num_rows, num_cols, sharey=True, squeeze=False)
+# axes = axes.flatten()
+# for x, gain_x in enumerate(gain):
+#     for d, (thresholds_x_d, gain_x_d) in enumerate(gain_x):
+#         axes[x].plot(thresholds_x_d, gain_x_d)
+#
+# plt.show()
